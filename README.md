64684b888c0065ec272b32827b23a2336c768da1
Charlie Moog <moogcharlie@gmail.com>
2020-12-24 22:21:51 -0600

    feat: add initial coder-cli version

---
+++ README.md
@@ -0,0 +1,3 @@
+# Homebrew Coder Tap
+
+A Homebrew Tap for Coder related utilities and applications.
---
+++ coder.rb
@@ -0,0 +1,14 @@
+class Coder < Formula
+  desc "A command-line tool for the Coder remote development platform"
+  homepage "https://github.com/cdr/coder-cli"
+  url "https://github.com/cdr/coder-cli/releases/download/v1.14.2/coder-cli-darwin-amd64.zip"
+  version "v1.14.2"
+  sha256 "69b69497a75ce19851681974aa7561af7c7061357f8616e24446e58795bf6e1f"
+  bottle :unneeded
+  def instal
+    bin.install "coder"
+  end
+  test do
+    system "#{bin}/coder", "--version"
+  end
+end
