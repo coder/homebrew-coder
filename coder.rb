b2a98784f3fd6424c4e373d42824bb4c6805a2d5
Charlie Moog <moogcharlie@gmail.com>
2020-12-25 09:26:15 -0600

    feat: add support for linuxbrew

--- coder.rb
+++ coder.rb
@@ -1,10 +1,17 @@
 class Coder < Formula
-  desc "A command-line tool for the Coder remote development platform"
+  desc "Command-line tool for the Coder remote development platform"
   homepage "https://github.com/cdr/coder-cli"
-  url "https://github.com/cdr/coder-cli/releases/download/v1.14.2/coder-cli-darwin-amd64.zip"
-  version "v1.14.2"
-  sha256 "69b69497a75ce19851681974aa7561af7c7061357f8616e24446e58795bf6e1f"
+  version "1.14.2"
   bottle :unneeded
+
+  if OS.mac?
+    url "https://github.com/cdr/coder-cli/releases/download/v1.14.2/coder-cli-darwin-amd64.zip"
+    sha256 "69b69497a75ce19851681974aa7561af7c7061357f8616e24446e58795bf6e1f"
+  else
+    url "https://github.com/cdr/coder-cli/releases/download/v1.14.2/coder-cli-linux-amd64.tar.gz"
+    sha256 "a670d99bea86a621eddebb41247cf0b699a2273bc5f0accfe3411c1253c16141"
+  end
+
   def install
     bin.install "coder"
   end
