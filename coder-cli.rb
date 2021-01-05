4d9b1c108cf6819e6e66584adb88d4de8fbe8611
Charlie Moog <moogcharlie@gmail.com>
2021-01-05 14:55:33 -0600

    bump coder-cli to v1.14.2+cli.1

--- coder-cli.rb
+++ coder-cli.rb
@@ -1,15 +1,15 @@
 class CoderCli < Formula
   desc "Command-line tool for the Coder remote development platform"
   homepage "https://github.com/cdr/coder-cli"
-  version "1.14.2"
+  version "1.14.2+cli.1"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/cdr/coder-cli/releases/download/v1.14.2/coder-cli-darwin-amd64.zip"
-    sha256 "69b69497a75ce19851681974aa7561af7c7061357f8616e24446e58795bf6e1f"
+    url "https://github.com/cdr/coder-cli/releases/download/v1.14.2%2Bc1.1/coder-cli-darwin-amd64.zip"
+    sha256 "41e475a2b228f5d646c488d537093eb444b235088e0e4c3f70ef47b5db0cb81d"
   else
-    url "https://github.com/cdr/coder-cli/releases/download/v1.14.2/coder-cli-linux-amd64.tar.gz"
-    sha256 "a670d99bea86a621eddebb41247cf0b699a2273bc5f0accfe3411c1253c16141"
+    url "https://github.com/cdr/coder-cli/releases/download/v1.14.2%2Bc1.1/coder-cli-linux-amd64.tar.gz"
+    sha256 "b0b99aa3872ef4b5e79187c8e9095e8c262766ee9b18f84790a0f5d5cfc78142"
   end
 
   def install
