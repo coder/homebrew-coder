ddac3500bf516ff019651c6d5aa6c3f219edd781
Charlie Moog <moogcharlie@gmail.com>
2021-01-29 20:04:04 +0000

    fixup! feat: add coder-cli nightly release

--- coder-cli-nightly.rb
+++ coder-cli-nightly.rb
@@ -6,10 +6,10 @@ class CoderCliNightly < Formula
 
   if OS.mac?
     url "https://github.com/cdr/coder-cli/releases/download/v1.16.0-rc.0/coder-cli-darwin-amd64.zip"
-    sha256 "7552b45f956c2a027c58beb0867d6df32d75291a0a5b6ad0c6bc9957306da989"
+    sha256 "283fbdbbd57ca75361ac89fb0300546483c326bb047e7cd96eb524c39241c5c5"
   else
     url "https://github.com/cdr/coder-cli/releases/download/v1.16.0-rc.0/coder-cli-linux-amd64.tar.gz"
-    sha256 "038ebbb4e338c2b308857691a1e6119a2419b68abfa9f95f9787db886703e0bc"
+    sha256 "7552b45f956c2a027c58beb0867d6df32d75291a0a5b6ad0c6bc9957306da989"
   end
 
   def install
