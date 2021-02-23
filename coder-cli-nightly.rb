4cc526ba4e82ec1135f21fab273a8c9fd98580e2
Charlie Moog <moogcharlie@gmail.com>
2021-02-22 21:32:59 -0600

    bump coder-cli to 1.16.0

--- coder-cli-nightly.rb
+++ coder-cli-nightly.rb
@@ -1,15 +1,15 @@
 class CoderCliNightly < Formula
   desc "Command-line tool for the Coder remote development platform, nightly release channel"
   homepage "https://github.com/cdr/coder-cli"
-  version "1.16.0-rc.0"
+  version "1.16.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/cdr/coder-cli/releases/download/v1.16.0-rc.0/coder-cli-darwin-amd64.zip"
-    sha256 "283fbdbbd57ca75361ac89fb0300546483c326bb047e7cd96eb524c39241c5c5"
+    url "https://github.com/cdr/coder-cli/releases/download/v1.16.0/coder-cli-darwin-amd64.zip"
+    sha256 "e2ae636f51245356992206c3e08f72fd6e937e1b17be804b82598a9dc2f0dee2"
   else
-    url "https://github.com/cdr/coder-cli/releases/download/v1.16.0-rc.0/coder-cli-linux-amd64.tar.gz"
-    sha256 "7552b45f956c2a027c58beb0867d6df32d75291a0a5b6ad0c6bc9957306da989"
+    url "https://github.com/cdr/coder-cli/releases/download/v1.16.0/coder-cli-linux-amd64.tar.gz"
+    sha256 "ae7c7af5c9dae2de2bfa24689c3d8afeecc95fa97eefc47ce7c324efeaabb724"
   end
 
   def install
--- coder-cli.rb
+++ coder-cli.rb
@@ -1,15 +1,15 @@
 class CoderCli < Formula
   desc "Command-line tool for the Coder remote development platform"
   homepage "https://github.com/cdr/coder-cli"
-  version "1.15.0"
+  version "1.16.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/cdr/coder-cli/releases/download/v1.15.0/coder-cli-darwin-amd64.zip"
-    sha256 "b3f2ac2f34818dbabc2789dc8b558c612bfc2da8afe62d2c7f197ff32ae8e9cf"
+    url "https://github.com/cdr/coder-cli/releases/download/v1.16.0/coder-cli-darwin-amd64.zip"
+    sha256 "e2ae636f51245356992206c3e08f72fd6e937e1b17be804b82598a9dc2f0dee2"
   else
-    url "https://github.com/cdr/coder-cli/releases/download/v1.15.0/coder-cli-linux-amd64.tar.gz"
-    sha256 "79db911672dd52426deafeea1b2360beaaeec5e5f6bb3fe1574c1ed9798655f5"
+    url "https://github.com/cdr/coder-cli/releases/download/v1.16.0/coder-cli-linux-amd64.tar.gz"
+    sha256 "ae7c7af5c9dae2de2bfa24689c3d8afeecc95fa97eefc47ce7c324efeaabb724"
   end
 
   def install
