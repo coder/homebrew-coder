92319a2b8eeee1131046cf22eccafd23e062d395
Charles Moog <moogcharlie@gmail.com>
2021-03-10 18:01:00 -0600

    Bump nightly (#14)
    * bump coder-cli-nightly
    
    * fixup! bump coder-cli-nightly

--- coder-cli-nightly.rb
+++ coder-cli-nightly.rb
@@ -1,15 +1,15 @@
 class CoderCliNightly < Formula
   desc "Command-line tool for the Coder remote development platform, nightly release channel"
   homepage "https://github.com/cdr/coder-cli"
-  version "1.16.0"
+  version "1.17.0-rc.1"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/cdr/coder-cli/releases/download/v1.16.0/coder-cli-darwin-amd64.zip"
-    sha256 "e2ae636f51245356992206c3e08f72fd6e937e1b17be804b82598a9dc2f0dee2"
+    url "https://github.com/cdr/coder-cli/releases/download/v1.17.0-rc.1/coder-cli-darwin-amd64.zip"
+    sha256 "efc6c6d7527c3f410c16402532daf0019035c5fb14d957134cdeb2904a29838f"
   else
-    url "https://github.com/cdr/coder-cli/releases/download/v1.16.0/coder-cli-linux-amd64.tar.gz"
-    sha256 "ae7c7af5c9dae2de2bfa24689c3d8afeecc95fa97eefc47ce7c324efeaabb724"
+    url "https://github.com/cdr/coder-cli/releases/download/v1.17.0-rc.1/coder-cli-linux-amd64.tar.gz"
+    sha256 "e69ea0ead0ed0499f4b74a99849277887e545b9ff413708d708b5a67a6bbd72e"
   end
 
   def install
