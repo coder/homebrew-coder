00a5a583c7f860a2b679c20d4855cfacfbd1666c
Jon Ayers <jon@coder.com>
2021-03-31 02:14:01 +0000

    fixup! chore: bump coder-cli to 1.17.2

--- coder-cli.rb
+++ coder-cli.rb
@@ -8,7 +8,7 @@ class CoderCli < Formula
     url "https://github.com/cdr/coder-cli/releases/download/v1.17.2/coder-cli-darwin-amd64.zip"
     sha256 "e69d7ef1fdfac58f6592772e9cced574289a8747e885ee6d45137c0da707571d"
   else
-    url "https://github.com/cdr/coder-cli/releases/download/v1.17.0/coder-cli-linux-amd64.tar.gz"
+    url "https://github.com/cdr/coder-cli/releases/download/v1.17.2/coder-cli-linux-amd64.tar.gz"
     sha256 "40a390d532e5889212b6a2477f0f1da9ebc93b1d092842c57b7a6c32d582b1e8"
   end
 
