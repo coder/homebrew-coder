24d84f73faba1cd255345284d043388ff012d535
Dean Sheather <dean@coder.com>
2021-08-27 04:21:43 +1000

    Update coder-cli.rb

--- coder-cli.rb
+++ coder-cli.rb
@@ -5,10 +5,10 @@ class CoderCli < Formula
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/cdr/coder-cli/releases/download/v1.21.0/coder-cli-darwin-amd64.zip"
+    url "https://github.com/cdr/coder-cli/releases/download/v1.22.0/coder-cli-darwin-amd64.zip"
     sha256 "6eb8479316c68bc2e3e6a90c3ca1b06c0307299a051b70f6222926e34d527e02"
   else
-    url "https://github.com/cdr/coder-cli/releases/download/v1.21.0/coder-cli-linux-amd64.tar.gz"
+    url "https://github.com/cdr/coder-cli/releases/download/v1.22.0/coder-cli-linux-amd64.tar.gz"
     sha256 "e39f6162e2c10ccb135975e19972df804e0fca4a1684d336956fcaaa5866b15b"
   end
 
