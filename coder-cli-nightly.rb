229b0240c48075b7b57a8230da1ec83dbb4b800f
Dean Sheather <dean@coder.com>
2021-11-19 05:53:46 +1000

    Update coder-cli-nightly.rb

--- coder-cli-nightly.rb
+++ coder-cli-nightly.rb
@@ -4,10 +4,10 @@ class CoderCliNightly < Formula
   version "1.25.0"
 
   if OS.mac?
-    url "https://github.com/cdr/coder-cli/releases/download/v1.24.0/coder-cli-darwin-amd64.zip"
+    url "https://github.com/cdr/coder-cli/releases/download/v1.25.0/coder-cli-darwin-amd64.zip"
     sha256 "8ae65247c807278f61f9a5559cef5e3236367fde5627a1769a1f28c7140eadaa"
   else
-    url "https://github.com/cdr/coder-cli/releases/download/v1.24.0/coder-cli-linux-amd64.tar.gz"
+    url "https://github.com/cdr/coder-cli/releases/download/v1.25.0/coder-cli-linux-amd64.tar.gz"
     sha256 "6281e2695158521c74bbfa860d8b4fa039e34f05c84a0111a9fb2f2a1e1d8ea1"
   end
 
