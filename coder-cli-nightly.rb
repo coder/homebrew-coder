4407f64aa79ca9fc4334630e823c342748a5e176
Dean Sheather <dean@deansheather.com>
2022-03-09 09:50:07 +1000

    chore: automate updating formulae (#30)

--- coder-cli-nightly.rb
+++ coder-cli-nightly.rb
@@ -1,14 +1,14 @@
 class CoderCliNightly < Formula
   desc "Command-line tool for the Coder remote development platform, nightly release channel"
   homepage "https://github.com/cdr/coder-cli"
-  version "1.27.0"
+  version "1.28.2"
 
   if OS.mac?
-    url "https://github.com/cdr/coder-cli/releases/download/v1.27.0/coder-cli-darwin-amd64.zip"
-    sha256 "13a17e97de04c4207920b1830e2aea9e8f8f0a4f0a8362c91fc35d5d218246e8"
+    url "https://github.com/cdr/coder-cli/releases/download/v1.28.2/coder-cli-darwin-amd64.zip"
+    sha256 "b0fbf3486d63892ffe718f12b6b418c49eecebdd34a9c713b53c1cf81385dd57"
   else
-    url "https://github.com/cdr/coder-cli/releases/download/v1.27.0/coder-cli-linux-amd64.tar.gz"
-    sha256 "1c79598e5dd29f0ba724a77d482389dfa629825905254ab31d7fc349bbd8076b"
+    url "https://github.com/cdr/coder-cli/releases/download/v1.28.2/coder-cli-linux-amd64.tar.gz"
+    sha256 "a3472dc7968affcbd9bf03fd14304eff365c0dad5d8ed732bd98744a2de880f6"
   end
 
   def install
