985de6359fba1af8db4a9cf9fe7cbca47a3d90a7
Colin Adler <colin1adler@gmail.com>
2022-07-20 18:41:32 -0500

    Revert "chore: release 1.31.2 (#48)"
    This reverts commit b4a19a381da6082d3a4d3be24845e9b2172fce66.

--- coder-cli.rb
+++ coder-cli.rb
@@ -1,14 +1,14 @@
 class CoderCli < Formula
   desc "Command-line tool for the Coder remote development platform"
   homepage "https://github.com/cdr/coder-cli"
-  version "1.31.2"
+  version "1.32.2"
 
   if OS.mac?
-    url "https://github.com/cdr/coder-cli/releases/download/v1.31.2/coder-cli-darwin-amd64.zip"
-    sha256 "ab9273cf21399856f12b7856fc918619d54aeaaeb177bbedee3ef400ab4db517"
+    url "https://github.com/cdr/coder-cli/releases/download/v1.32.2/coder-cli-darwin-amd64.zip"
+    sha256 "23cf0daea7af10701d969fd0b6e1d19b9a228335a8dcc13b6701f8bc94b7b99a"
   else
-    url "https://github.com/cdr/coder-cli/releases/download/v1.31.2/coder-cli-linux-amd64.tar.gz"
-    sha256 "1311791ddec5246b2ccfef9ed01658a854b8199a27a03654dc1321ff81ee5c85"
+    url "https://github.com/cdr/coder-cli/releases/download/v1.32.2/coder-cli-linux-amd64.tar.gz"
+    sha256 "3a9464884e007fb423768b0b90daa050c2fa7de1122c9ba72f3cc8ca5bf91865"
   end
 
   def install
