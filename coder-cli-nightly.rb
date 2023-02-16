cfcb39c15294c0810ee8ad049a0d12fd8ce7a829
Coder CI <ci@coder.com>
2023-02-16 22:24:50 +0000

    chore: release 1.39.2

--- coder-cli-nightly.rb
+++ coder-cli-nightly.rb
@@ -1,14 +1,14 @@
 class CoderCliNightly < Formula
   desc "Command-line tool for the Coder remote development platform, nightly release channel"
   homepage "https://github.com/cdr/coder-cli"
-  version "1.39.1"
+  version "1.39.2"
 
   if OS.mac?
-    url "https://github.com/cdr/coder-cli/releases/download/v1.39.1/coder-cli-darwin-amd64.zip"
-    sha256 "74cf000a932b932dd025bd3da52f382916d45c5b5a0b143ef05ab6db10dcba16"
+    url "https://github.com/cdr/coder-cli/releases/download/v1.39.2/coder-cli-darwin-amd64.zip"
+    sha256 "911b39262951cc2a6fed59716073522b7fb06c27c99704f32ba9fbdd9aa08271"
   else
-    url "https://github.com/cdr/coder-cli/releases/download/v1.39.1/coder-cli-linux-amd64.tar.gz"
-    sha256 "fc08fe979f0d57a6215f2c8397e3868c7bff2b159817c2d29f511b013f2ded1a"
+    url "https://github.com/cdr/coder-cli/releases/download/v1.39.2/coder-cli-linux-amd64.tar.gz"
+    sha256 "9aa8faa832af303e5ccacb743113d584b22ea570dc4f660d51f833f0c8805153"
   end
 
   def install
