f2ff8a47ba1f141a9054bc82a86cbbd4f1ea47f4
McKayla Washburn <mckayla@hey.com>
2023-08-29 11:51:39 -0600

    and nice script cleanup

--- ci/update-v1.sh
+++ ci/update-v1.sh
@@ -11,15 +11,8 @@ version="$1"
 darwin_sha="$(echo "$2" | tr "[:upper:]" "[:lower:]")"
 linux_sha="$(echo "$3" | tr "[:upper:]" "[:lower:]")"
 
-# Get the old version to use in our find/replace.
-old_version="$(sed -n "s/.*version \"\(.*\)\".*/\1/p" "../Formula/coder@1.rb")"
-if [[ "$old_version" == "" ]]; then
-  echo "Could not determine the old version of the formula..." >&2
-  exit 1
-fi
-
 # Replace version
-sed -i "s/${old_version//./\.}/${version//./\.}/g" "../Formula/coder@1.rb"
+sed -i "s/version \"[0-9.]*\"/version \"$version\"/g" "../Formula/coder@1.rb"
 
 # Update macOS hash
 sed -zi "s/sha256 \"[a-f0-9]*\"/sha256 \"$darwin_sha\"/1" "../Formula/coder@1.rb"
