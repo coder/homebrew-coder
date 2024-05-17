bac778c94d52ba52f701181b537f8f25c174be3b
Asher <ash@coder.com>
2024-05-17 18:23:59 +0000

    chore: release 1.44.5 (#135)

--- Formula/coder@1.rb
+++ Formula/coder@1.rb
@@ -1,14 +1,14 @@
 class CoderAT1 < Formula
   desc "Command-line tool for the Coder remote development platform"
   homepage "https://github.com/coder/coder-v1-cli"
-  version "1.44.4"
+  version "1.44.5"
 
   if OS.mac?
     url "https://github.com/coder/coder-v1-cli/releases/download/v#{version}/coder-cli-darwin-amd64.zip"
-    sha256 "7e92a09972264cd1fac63f1327d69f119425a1bb0ca155f9ff84ef8ca251b7e5"
+    sha256 "680fbd91328f1f1025d4591d540fa6ae72eb7b10136b9b3447596876fe0ea8a8"
   else
     url "https://github.com/coder/coder-v1-cli/releases/download/v#{version}/coder-cli-linux-amd64.tar.gz"
-    sha256 "787385a416fdfa1b5b2dfbc0c73f128b9d259e0701f56db73ca6156ae8078c4a"
+    sha256 "5603e60fcf236d6ddcd72dd84b2c1ad7f5e784597d3b04b30766b34e72527a8b"
   end
 
   keg_only :versioned_formula
