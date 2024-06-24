d66f81caa5ac543b2e0f8f8aab60e1ff6104ff99
McKayla Washburn <mckayla@hey.com>
2024-06-24 11:04:00 -0600

    chore: put braces around variable references

--- scripts/update-v1.sh
+++ scripts/update-v1.sh
@@ -11,10 +11,10 @@ darwin_sha="$(echo "$2" | tr "[:upper:]" "[:lower:]")"
 linux_sha="$(echo "$3" | tr "[:upper:]" "[:lower:]")"
 
 # Replace version
-sed -i "s/version \"[0-9.]*\"/version \"$version\"/g" "../Formula/coder@1.rb"
+sed -i "s/version \"[0-9.]*\"/version \"${version}\"/g" "../Formula/coder@1.rb"
 
 # Update macOS hash
-sed -zi "s/sha256 \"[a-f0-9]*\"/sha256 \"$darwin_sha\"/1" "../Formula/coder@1.rb"
+sed -zi "s/sha256 \"[a-f0-9]*\"/sha256 \"${darwin_sha}\"/1" "../Formula/coder@1.rb"
 
 # Update Linux hash
-sed -zi "s/sha256 \"[a-f0-9]*\"/sha256 \"$linux_sha\"/2" "../Formula/coder@1.rb"
+sed -zi "s/sha256 \"[a-f0-9]*\"/sha256 \"${linux_sha}\"/2" "../Formula/coder@1.rb"
