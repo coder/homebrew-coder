6df88ca3f7b97099caa42cfb786cd17b1f30ab90
Kayla Washburn-Love <mckayla@hey.com>
2024-06-24 14:10:53 -0600

    chore: put braces around variable references (#143)
    Make shellcheck happy, since that got added to the upstream action.
    
    Unblocks #142

--- scripts/update-v2.sh
+++ scripts/update-v2.sh
@@ -12,13 +12,13 @@ darwin_intel_sha="$(echo "$3" | tr "[:upper:]" "[:lower:]")"
 linux_sha="$(echo "$4" | tr "[:upper:]" "[:lower:]")"
 
 # Replace version
-sed -i "s/version \"[0-9.]*\"/version \"$version\"/g" "../Formula/coder.rb"
+sed -i "s/version \"[0-9.]*\"/version \"${version}\"/g" "../Formula/coder.rb"
 
 # Update macOS ARM hash
-sed -zi "s/sha256 \"[a-f0-9]*\"/sha256 \"$darwin_arm_sha\"/1" "../Formula/coder.rb"
+sed -zi "s/sha256 \"[a-f0-9]*\"/sha256 \"${darwin_arm_sha}\"/1" "../Formula/coder.rb"
 
 # Update macOS Intel hash
-sed -zi "s/sha256 \"[a-f0-9]*\"/sha256 \"$darwin_intel_sha\"/2" "../Formula/coder.rb"
+sed -zi "s/sha256 \"[a-f0-9]*\"/sha256 \"${darwin_intel_sha}\"/2" "../Formula/coder.rb"
 
 # Update Linux hash
-sed -zi "s/sha256 \"[a-f0-9]*\"/sha256 \"$linux_sha\"/3" "../Formula/coder.rb"
+sed -zi "s/sha256 \"[a-f0-9]*\"/sha256 \"${linux_sha}\"/3" "../Formula/coder.rb"
