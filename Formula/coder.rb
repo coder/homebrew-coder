a18fb77a13da05e13bb46cde425496d39b1f7af4
cdrci <78873720+cdrci@users.noreply.github.com>
2024-09-04 03:31:44 +1000

    coder 2.15.0 (#158)
    This automatic PR was triggered by the release of Coder v2.15.0
    
    Co-authored-by: Coder CI <ci@coder.com>

--- Formula/coder.rb
+++ Formula/coder.rb
@@ -1,19 +1,19 @@
 class Coder < Formula
   desc "Provisions remote development environments via Terraform"
   homepage "https://github.com/coder/coder"
-  version "2.14.2"
+  version "2.15.0"
 
   if OS.mac?
     if Hardware::CPU.arm?
       url "https://github.com/coder/coder/releases/download/v#{version}/coder_#{version}_darwin_arm64.zip"
-      sha256 "fa5156cfaa83ba1b43367ec3203fe59a9aa5b691307ed54fdd4fb60ac7953276"
+      sha256 "50fd3c0e7711bccd4d8ed30e7da9c39d71b248ad51ac252d6b995b22f27bbeda"
     else
       url "https://github.com/coder/coder/releases/download/v#{version}/coder_#{version}_darwin_amd64.zip"
-      sha256 "bb2b2540801739139276dca22d251cc44c18ccafa20eea7cfe3448ef8406704a"
+      sha256 "01b5bdd9131a3dfbacbdee43c51693de7a5e376cd5ceb04e04bdd718df36df92"
     end
   else
     url "https://github.com/coder/coder/releases/download/v#{version}/coder_#{version}_linux_amd64.tar.gz"
-    sha256 "420973f05f344080b058e5700c42de81ec181e43e186b4c4b86289822c369988"
+    sha256 "ccce65def90b2ae0dd6474e055361fbdf61318b0a90e70361990e1e4f2d0f6bb"
   end
 
   def install
