b65f50f5734ef1b67e58e3a8e03618d42eb047f9
cdrci <78873720+cdrci@users.noreply.github.com>
2025-02-18 23:58:40 +1100

    Coder Desktop v0.1.0-6-g250017b (#181)
    This automatic PR was triggered by the release of Coder Desktop
    v0.1.0-6-g250017b
    
    Co-authored-by: Coder CI <ci@coder.com>

---
+++ Casks/coder-desktop-preview.rb
@@ -0,0 +1,14 @@
+cask "coder-desktop-preview" do
+  version "0.1.0-6-g250017b"
+  sha256 :no_check
+
+  url "https://github.com/coder/coder-desktop-macos/releases/download/preview/Coder.Desktop.dmg"
+  name "Coder Desktop"
+  desc "Coder Desktop client"
+  homepage "https://github.com/coder/coder-desktop-macos"
+
+  conflicts_with cask: "coder/coder/coder-desktop"
+  depends_on macos: ">= :sonoma"
+
+  app "Coder Desktop.app"
+end
