2b61013817aa84116bfc4293e6f66f1e0ecbe517
cdrci <78873720+cdrci@users.noreply.github.com>
2025-02-24 15:00:18 +1100

    Coder Desktop v0.1.0-12-gf970cfc (#188)
    This automatic PR was triggered by the release of Coder Desktop
    v0.1.0-12-gf970cfc
    
    Co-authored-by: Coder CI <ci@coder.com>

--- Casks/coder-desktop-preview.rb
+++ Casks/coder-desktop-preview.rb
@@ -1,5 +1,5 @@
 cask "coder-desktop-preview" do
-  version "0.1.0-11-g3442516"
+  version "0.1.0-12-gf970cfc"
   sha256 :no_check
 
   url "https://github.com/coder/coder-desktop-macos/releases/download/preview/Coder.Desktop.dmg"
