35dd5002ba5587f9a4f2fa24fde245d58a173b79
Coder CI <ci@coder.com>
2025-03-27 07:53:08 +0000

    Coder Desktop v0.2.0-15-gd95289b

--- Casks/coder-desktop-preview.rb
+++ Casks/coder-desktop-preview.rb
@@ -1,5 +1,5 @@
 cask "coder-desktop-preview" do
-  version "0.2.0-13-gf53a99f"
+  version "0.2.0-15-gd95289b"
   sha256 :no_check
 
   url "https://github.com/coder/coder-desktop-macos/releases/download/preview/Coder-Desktop.pkg"
