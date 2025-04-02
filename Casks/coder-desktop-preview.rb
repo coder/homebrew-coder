da6e863526bdd2de26eca189b690bbf84c05cde4
cdrci <78873720+cdrci@users.noreply.github.com>
2025-04-02 18:46:33 +1100

    Coder Desktop v0.2.0-22-gfe20801 (#219)

--- Casks/coder-desktop-preview.rb
+++ Casks/coder-desktop-preview.rb
@@ -1,5 +1,5 @@
 cask "coder-desktop-preview" do
-  version "0.2.0-21-g1fd5855"
+  version "0.2.0-22-gfe20801"
   sha256 :no_check
 
   url "https://github.com/coder/coder-desktop-macos/releases/download/preview/Coder-Desktop.pkg"
