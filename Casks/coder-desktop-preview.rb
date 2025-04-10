9a543965a9f4d6be712655a483d85f990d27a8ea
cdrci <78873720+cdrci@users.noreply.github.com>
2025-04-10 14:27:18 +1000

    Coder Desktop v0.3.0-1-g918bacd (#223)
    This automatic PR was triggered by the release of Coder Desktop
    v0.3.0-1-g918bacd
    
    Co-authored-by: Coder CI <ci@coder.com>

--- Casks/coder-desktop-preview.rb
+++ Casks/coder-desktop-preview.rb
@@ -1,5 +1,5 @@
 cask "coder-desktop-preview" do
-  version "0.2.0-25-g8067574"
+  version "0.3.0-1-g918bacd"
   sha256 :no_check
 
   url "https://github.com/coder/coder-desktop-macos/releases/download/preview/Coder-Desktop.pkg"
