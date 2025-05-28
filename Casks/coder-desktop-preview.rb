22d97f65b160fa8dde938fc0540489c700f5a1b9
cdrci <78873720+cdrci@users.noreply.github.com>
2025-05-28 15:49:05 +1000

    Coder Desktop 0.5.0.2 (#248)
    This automatic PR was triggered by the release of Coder Desktop 0.5.0.2
    
    Co-authored-by: Coder CI <ci@coder.com>

--- Casks/coder-desktop-preview.rb
+++ Casks/coder-desktop-preview.rb
@@ -1,5 +1,5 @@
 cask "coder-desktop-preview" do
-  version "0.5.0-1-gb2da490"
+  version "0.5.0.2"
   sha256 :no_check
 
   url "https://github.com/coder/coder-desktop-macos/releases/download/preview/Coder-Desktop.pkg"
