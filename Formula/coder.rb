980e8329e981a891b4739140584f478185b377f6
Coder CI <ci@coder.com>
2025-12-01 22:20:01 +0000

    coder 2.26.6

--- Formula/coder.rb
+++ Formula/coder.rb
@@ -1,19 +1,19 @@
 class Coder < Formula
   desc "Provisions remote development environments via Terraform"
   homepage "https://github.com/coder/coder"
-  version "2.28.4"
+  version "2.26.6"
 
   if OS.mac?
     if Hardware::CPU.arm?
       url "https://github.com/coder/coder/releases/download/v#{version}/coder_#{version}_darwin_arm64.zip"
-      sha256 "f80c3e49323cf3606ccc5f4fa0b765ebdb768f0a9c9d79aed421016785a70271"
+      sha256 "22bbe9f5c3a1fb549a04dc09e181f455fb4a65456627589a735f75af6e5e2721"
     else
       url "https://github.com/coder/coder/releases/download/v#{version}/coder_#{version}_darwin_amd64.zip"
-      sha256 "626da42b8c10a2f5bf0281c24bfd4377aa019935ad6e7eea1b68b41c6c7d10df"
+      sha256 "9920a0964d3bb6d6a6122e508788642dad4c6e9b2e2f7b56eb2c91082b9c358a"
     end
   else
     url "https://github.com/coder/coder/releases/download/v#{version}/coder_#{version}_linux_amd64.tar.gz"
-    sha256 "bc2322d53a935ddb2c551a035e51805f899207479e7f8069373b4e781136071f"
+    sha256 "b4eed20f00b7aeb13f94e4f9ab063347a32ff2a420a4952eed72385cd0523a25"
   end
 
   def install
