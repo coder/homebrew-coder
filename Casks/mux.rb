3cdd9a26d618f6dbf4df19dde01ff2247b6028aa
Thomas Kosiewski <tk@coder.com>
2026-02-05 15:20:08 +0000

    mux: add verified to url stanza

--- Casks/mux.rb
+++ Casks/mux.rb
@@ -7,7 +7,8 @@ cask "mux" do
          x86_64_linux: "76e73b8aa2cd67d8618aa6d56060554967704c58f1393133b7b8e783b6ca85f4",
          arm64_linux:  "4e0b9fefc123a3fb81fbd91d9e787bdb73813779985dc5cf3f572052def07a83"
 
-  url "https://github.com/coder/mux/releases/download/v#{version}/mux-#{version}-#{arch}.dmg"
+  url "https://github.com/coder/mux/releases/download/v#{version}/mux-#{version}-#{arch}.dmg",
+      verified: "github.com/coder/mux/"
   name "Mux"
   desc "Desktop app for parallel agent workflows"
   homepage "https://mux.coder.com/"
