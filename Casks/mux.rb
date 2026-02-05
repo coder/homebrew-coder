bdcd49a21f2e4474ba274423c8082797442272c4
Thomas Kosiewski <tk@coder.com>
2026-02-05 15:00:21 +0000

    mux: fix sha256 for Linux audit

--- Casks/mux.rb
+++ Casks/mux.rb
@@ -2,8 +2,10 @@ cask "mux" do
   arch arm: "arm64", intel: "x64"
 
   version "0.15.2"
-  sha256 arm:   "4e0b9fefc123a3fb81fbd91d9e787bdb73813779985dc5cf3f572052def07a83",
-         intel: "76e73b8aa2cd67d8618aa6d56060554967704c58f1393133b7b8e783b6ca85f4"
+  sha256 arm:          "4e0b9fefc123a3fb81fbd91d9e787bdb73813779985dc5cf3f572052def07a83",
+         x86_64:       "76e73b8aa2cd67d8618aa6d56060554967704c58f1393133b7b8e783b6ca85f4",
+         x86_64_linux: :no_check,
+         arm64_linux:  :no_check
 
   url "https://github.com/coder/mux/releases/download/v#{version}/mux-#{version}-#{arch}.dmg"
   name "Mux"
