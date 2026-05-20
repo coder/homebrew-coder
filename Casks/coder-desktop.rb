55bebb2ffa53c2914712e9719c17a128f234217d
cdrci <78873720+cdrci@users.noreply.github.com>
2026-05-20 16:14:05 +1000

    Coder Desktop v0.8.1 (#360)
    This automatic PR was triggered by the release of Coder Desktop v0.8.1
    
    ---------
    
    Co-authored-by: Coder CI <ci@coder.com>
    Co-authored-by: Ethan Dickson <ethanndickson@gmail.com>

--- Casks/coder-desktop.rb
+++ Casks/coder-desktop.rb
@@ -1,6 +1,6 @@
 cask "coder-desktop" do
-  version "0.8.0"
-  sha256 "e683fe7dcd3abb6485a6ebb0668a0fd72ed04efb0d11163cc22b5f3eab85ff88"
+  version "0.8.1"
+  sha256 "b4187930ff59a4a7eee65cf79fbc2cadc6b94e3d6eea505a740cd27880d23c57"
 
   url "https://github.com/coder/coder-desktop-macos/releases/download/v#{version}/Coder-Desktop.pkg"
   name "Coder Desktop"
@@ -20,8 +20,8 @@ cask "coder-desktop" do
             login_item: "Coder Desktop"
 
   zap delete: [
+        "/var/root/Library/Application Support/com.coder.Coder-Desktop/coder-darwin-amd64",
         "/var/root/Library/Application Support/com.coder.Coder-Desktop/coder-darwin-arm64",
-        "/var/root/Library/Application Support/com.coder.Coder-Desktop/coder-darwin_amd64",
         "/var/root/Library/Containers/com.Coder-Desktop.VPN/Data/Documents/coder-vpn.dylib",
       ],
       trash:  [
